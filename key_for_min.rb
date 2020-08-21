00f33f816c45ce9e9bc3ce695457f1a100b1695b
C. Chang <chloechang416@gmail.com>
2020-08-21 04:53:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
-    lowest_key = nil
-    lowest_value = nil
-    hash.each do |k, v|
-        if lowest_value == nil || v < lowest_value
-          lowest_value = v
-          lowest_key = k
-        end
-      end
-lowest_value
-end
+  it "does not call the `#sort_by` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:sort_by)
+
+    key_for_min_value(hash)
+  end
